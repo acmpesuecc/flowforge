29e062f2cca20c2ee21be6f26b5993dd32a2818f
devadigapratham <devadigapratham8@gmail.com>
2024-09-28 15:30:10 +0530

    Update .gitignore to exclude node_modules and .next

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/prathamesh/Projects/flowforge/src/app/layout\":[\"static/media/7e6a2e30184bb114-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
